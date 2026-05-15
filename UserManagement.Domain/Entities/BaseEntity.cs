20acb0b97ce679b9fbd8a0e73d30e0460bbc3d7d
shimworks <shimworks.tk@gmail.com>
2026-05-15 11:25:24 -0300

    Add abstract class for entities

---
+++ UserManagement.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace UserManagement.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    protected void SetUpdatedAt() => UpdatedAt = DateTime.UtcNow;
+}
