40d75c45f91390b45a05136eacd8ad80585a3260
shimworks <shimworks.tk@gmail.com>
2026-05-18 16:55:52 -0300

    Add DeleteUserCommand Validator

---
+++ UserManagement.Application/Users/Commands/Validators/DeleteUserCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace UserManagement.Application.Users.Commands.Validators;
+
+public sealed class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+{
+    public DeleteUserCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
