ec54c6228017c1d3cdbbd24b1bb330189f95d752
shimworks <shimworks.tk@gmail.com>
2026-05-18 17:00:35 -0300

    Add Query Validators

---
+++ UserManagement.Application/Users/Queries/ListUsersQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using UserManagement.Application.Common.DTOs;
+using UserManagement.Application.Users.DTOs;
+using UserManagement.Domain.Interfaces;
+
+namespace UserManagement.Application.Users.Queries;
+
+public sealed class ListUsersQueryHandler
+    : IRequestHandler<ListUsersQuery, PagedResponse<UserResponse>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public ListUsersQueryHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<PagedResponse<UserResponse>> Handle(
+        ListUsersQuery request,
+        CancellationToken cancellationToken)
+    {
+        // Executa contagem e listagem em paralelo para melhor performance
+        var usersTask = _userRepository.ListAsync(request.Page, request.PageSize, cancellationToken);
+        var countTask = _userRepository.CountAsync(cancellationToken);
+
+        await Task.WhenAll(usersTask, countTask);
+
+        var items = _mapper.Map<IEnumerable<UserResponse>>(await usersTask);
+        var total = await countTask;
+
+        return new PagedResponse<UserResponse>(
+            Items: items,
+            Page: request.Page,
+            PageSize: request.PageSize,
+            TotalCount: total);
+    }
+}
---
+++ UserManagement.Application/Users/Queries/Validators/ListUsersQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace UserManagement.Application.Users.Queries.Validators;
+
+public sealed class GetUserByIdQueryValidator : AbstractValidator<GetUserByIdQuery>
+{
+    public GetUserByIdQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.");
+    }
+}
