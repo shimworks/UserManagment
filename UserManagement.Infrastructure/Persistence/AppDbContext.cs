99098b2f914a16cec57c4c6d12a3f01ee570231b
shimworks <shimworks.tk@gmail.com>
2026-05-18 17:46:12 -0300

    Add AppDbcontext

---
+++ UserManagement.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Application.Common.Interfaces;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Infrastructure.Persistence;
+
+public sealed class AppDbContext : DbContext, IUnitOfWork
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public async Task<int> CommitAsync(CancellationToken cancellationToken = default)
+        => await SaveChangesAsync(cancellationToken);
+}
