6c875c008f87d482c855b57c942d9176d519b072
shimworks <shimworks.tk@gmail.com>
2026-05-18 16:57:10 -0300

    Add GetUserByIdQuery

---
+++ UserManagement.Application/Users/Queries/GetUserByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using UserManagement.Application.Users.DTOs;
+using UserManagement.Domain.Entities;
+using UserManagement.Domain.Exceptions;
+using UserManagement.Domain.Interfaces;
+
+namespace UserManagement.Application.Users.Queries;
+
+public sealed class GetUserByIdQueryHandler
+    : IRequestHandler<GetUserByIdQuery, UserResponse>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public GetUserByIdQueryHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<UserResponse> Handle(
+        GetUserByIdQuery request,
+        CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (user is null)
+            throw new NotFoundException(nameof(User), request.Id);
+
+        return _mapper.Map<UserResponse>(user);
+    }
+}
