a2773221af1bc00183763a693712d99580fb1642
shimworks <shimworks.tk@gmail.com>
2026-05-18 16:53:15 -0300

    Add DeleteUserCommandHandler

---
+++ UserManagement.Application/Users/Commands/DeleteUserCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using UserManagement.Domain.Entities;
+using UserManagement.Domain.Exceptions;
+using UserManagement.Domain.Interfaces;
+
+namespace UserManagement.Application.Users.Commands;
+
+public sealed class DeleteUserCommandHandler
+    : IRequestHandler<DeleteUserCommand, Unit>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteUserCommandHandler(
+        IUserRepository userRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(
+        DeleteUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (user is null)
+            throw new NotFoundException(nameof(User), request.Id);
+
+        // Soft delete: chama o metodo de dominio que altera IsActive e UpdatedAt
+        user.Deactivate();
+
+        _userRepository.Update(user);
+        await _unitOfWork.CommitAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
