202e2d6b2d7fbfd99d1d5de5d9dfe5a16f7ce2a5
shimworks <shimworks.tk@gmail.com>
2026-05-18 17:31:37 -0300

    Add Trabsient for Balidation

--- UserManagement.API/Program.cs
+++ UserManagement.API/Program.cs
@@ -1,8 +1,11 @@
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
 var app = builder.Build();
 
