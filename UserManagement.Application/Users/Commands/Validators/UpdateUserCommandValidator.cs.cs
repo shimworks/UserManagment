98a7933a36be0604f42b341639694de12bc8b521
shimworks <shimworks.tk@gmail.com>
2026-05-18 16:52:00 -0300

    Add Validators for UpdateUser

---
+++ UserManagement.Application/Users/Commands/Validators/UpdateUserCommandValidator.cs.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace UserManagement.Application.Users.Commands.Validators;
+
+public sealed class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Email is not valid.")
+            .MaximumLength(255).WithMessage("Email must not exceed 255 characters.");
+    }
+}
