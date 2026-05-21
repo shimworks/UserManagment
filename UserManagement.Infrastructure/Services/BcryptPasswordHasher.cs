cfb3dfc7249ca0e22d9a7233fe4055e67d1a337f
shimworks <shimworks.tk@gmail.com>
2026-05-21 09:59:55 -0300

    Add BCrypt

---
+++ UserManagement.Infrastructure/Services/BcryptPasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using UserManagement.Application.Common.Interfaces;
+
+namespace UserManagement.Infrastructure.Services;
+
+public sealed class BcryptPasswordHasher : IPasswordHasher
+{
+    public string Hash(string plainPassword)
+        => BCrypt.Net.BCrypt.HashPassword(plainPassword, workFactor: 12);
+
+    public bool Verify(string plainPassword, string hash)
+        => BCrypt.Net.BCrypt.Verify(plainPassword, hash);
+}
